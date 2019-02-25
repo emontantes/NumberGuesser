a92a379e8f8f7d0fa289f8337e000ebde169f370
Esteban Montantes <esmo.ledev@gmail.com>
2019-02-25 16:57:12 -0700

    created app name, version and author

--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ namespace NumberGuesser
         //Entry Point Method
         public static void Main(string[] args)
         {
-
-            //printing my name
-            string name = "Esteban Montantes";
-            int age = 30;
-
-            //Start Application Here
-            Console.WriteLine(name+ " is "+age);
-            //Using Dynamic Variables
-            Console.WriteLine("{0} is {1}", name, age);
-        }
+            //heading for version and author name
+            //set app variables
+            //this is 0
+            string appName = "Number Guesser";
+            //this is 1
+            string appVersion = "1.0.0";
+            //this is 2
+            string appAuthor = "Esteban Montantes";
+            //printing app name, version and author
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+        } 
     }
 }
