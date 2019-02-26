00ecf499e53b7c87a76c2e10e3b55190847703b8
Esteban Montantes <esmo.ledev@gmail.com>
2019-02-25 18:46:58 -0700

    Play again? ability

--- Program.cs
+++ Program.cs
@@ -45,56 +45,82 @@ namespace NumberGuesser
             //setting guess variable
             int guessNumber = 0;
 
-            //asking user for their number to guess
-            Console.WriteLine("Guess a number between 1 and 10");
 
-                
 
-            //while guess is not correct
-            while (guessNumber != correctNumber) {
-                //Get user guess
-                string inputGuess = Console.ReadLine();
+            while (true) {
+                //ask user for their number to guess
+                Console.WriteLine("Guess a number between 1 and 10...");
 
-                //Require numbers ONLY from user input
-                if (!int.TryParse(inputGuess, out guessNumber))
+                //while guess is wrong
+                while (guessNumber != correctNumber)
                 {
-                    //notify correct number
-                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    //Get user guess
+                    string inputGuess = Console.ReadLine();
 
-                    //guess is correct message
-                    Console.WriteLine("Please use real numbers 1 through 10!");
+                    //Require numbers ONLY from user input
+                    if (!int.TryParse(inputGuess, out guessNumber))
+                    {
+                        //notify correct number
+                        Console.ForegroundColor = ConsoleColor.Yellow;
 
-                    //reset text color
-                    Console.ResetColor();
+                        //guess is correct message
+                        Console.WriteLine("Please use real numbers 1 through 10!");
 
-                    //throws error message BUT can still RUN
+                        //reset text color
+                        Console.ResetColor();
+
+                        //throws error message BUT can still RUN
+                        continue;
+                    }
+
+                    //cast to integer and put into guess variable
+                    guessNumber = Int32.Parse(inputGuess);
+
+                    //notify wrong number
+                    if (guessNumber != correctNumber)
+                    {
+                        //change text color
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        //print wrong number message
+                        Console.WriteLine("Wrong number, please try agaiin.");
+
+                        //reset text color
+                        Console.ResetColor();
+                    } // if loop end
+                } //wrong guess loop end
+
+                //notify correct number
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                //guess is correct message
+                Console.WriteLine("Beautiful number, that's correct!");
+
+                //reset text color
+                Console.ResetColor();
+
+                // ask to play again
+                Console.WriteLine("Play Again? [Y or N]");
+
+                //get answer input and set tp uppercase
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
                     continue;
                 }
 
-                //cast to integer and put into guess variable
-                guessNumber = Int32.Parse(inputGuess);
+                else if (answer == "N")
+                {
+                    return;
+                }
 
-                //notify wrong number
-                if(guessNumber != correctNumber) {
-                    //change text color
-                    Console.ForegroundColor = ConsoleColor.Red;
+                else
+                {
+                    return;
+                }
+            } // while true end
 
-                    //print wrong number message
-                    Console.WriteLine("Wrong number, please try agaiin.");
-
-                    //reset text color
-                    Console.ResetColor();
-                } // if loop end
-            } //while loop end
-
-            //notify correct number
-            Console.ForegroundColor = ConsoleColor.Green;
-
-            //guess is correct message
-            Console.WriteLine("Beautiful number, that's correct!");
-
-            //reset text color
-            Console.ResetColor();
         } 
     } //main class end
 }
